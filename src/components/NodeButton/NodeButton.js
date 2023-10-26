a74ac381d2c218f59735852be726b7564d42d67a
jaime <jabram617@gmail.com>
2023-10-26 16:42:26 -0400

    Style NodeButton with icon
    It's been well over 90 minutes but can't stop won't stop too much fun :)

--- src/components/NodeButton/NodeButton.js
+++ src/components/NodeButton/NodeButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./NodeButton.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 const NodeButton = ({ isOn }) => {
-  return <button className={styles.nodeButton}>{isOn ? "yes" : "no"}</button>;
+  return (
+    <button className={styles.nodeButton}>
+      {isOn && <FontAwesomeIcon icon={faCircle} className={styles.dot} />}
+    </button>
+  );
 };
 
 NodeButton.propTypes = {
